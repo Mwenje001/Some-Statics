2507dec78797bf1fc67202a92dd2879f5ba8922c
Mwenje001 <mwenje72@gmail.com>
2022-12-07 10:32:09 +0300

    Be happy

---
+++ add.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void add(int a, int b)
+{
+
+	printf("The sum is : %d\n",  a+b);
+
+}
---
+++ sub.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void sud(int a, int b)
+{
+
+	printf ("The substraction is : %d\n", a-b);
+
+
+}
